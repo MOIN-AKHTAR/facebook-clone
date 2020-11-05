aa3e929895bde43af545bac253028c0b07780ff3
MOIN-AKHTAR <moinakhter179@gmail.com>
2020-11-05 21:58:29 +0500

    A little Bit Improvement

--- src/Favourites.jsx
+++ src/Favourites.jsx
@@ -4,9 +4,9 @@ export const Favourites = () => {
     return (
         <div className="favourites" >
           <h1>Favourites</h1>
-        <iframe width="100%" height="315" src="https://www.youtube.com/embed/-7vd6wswQyU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <iframe width="100%" height="315" src="https://www.youtube.com/embed/hEw4KtUgp1I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <iframe width="100%" height="315" src="https://www.youtube.com/embed/TuRCIFrL4Ew" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/-7vd6wswQyU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="frame1"></iframe>
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/hEw4KtUgp1I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="frame2"></iframe>
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/TuRCIFrL4Ew" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="frame3"></iframe>
         </div>
     )
 }
